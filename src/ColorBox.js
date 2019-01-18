6c2ece9d7b8d7d836ed98f2a94b83002fef9a5e6
James Lotruglio <jimlotruglio@yahoo.com>
2019-01-18 00:20:34 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,7 +5,11 @@ export default class ColorBox extends Component {
   render() {
 
       if(this.props.opacity >= 0.2){
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> d528d5d9e8f091962dc0d1719bcff79010bfa12d
         return (
       <div className="color-box" style={{opacity: this.props.opacity}}>
 
